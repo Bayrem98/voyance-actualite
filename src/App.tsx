ec9d0dba1591b6324058a892a210fb6da3aed1bd
bayrem98 <bayremsayeh98@gmail.com>
2024-11-28 05:37:40 +0100

    add new page refer & put auto navig for pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,49 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useNavigate } from "react-router-dom";
 
 import Navbard from "./component/parts/Navbard";
 import Footer from "./component/parts/Footer";
 import Home from "./component/pages/Home";
+import Referentiel from "./component/pages/Referentiel";
 
 function App() {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    // Temps pour changer de page (en millisecondes)
+    const interval = 9000; // 9 secondes
+
+    // Logique pour changer automatiquement de page
+    const pages = ["/1", "/2"]; // Liste des chemins
+    let currentPageIndex = 0;
+
+    const intervalId = setInterval(() => {
+      currentPageIndex = (currentPageIndex + 1) % pages.length;
+      navigate(pages[currentPageIndex]);
+    }, interval);
+
+    // Nettoyer l'intervalle lorsque le composant est démonté
+    return () => clearInterval(intervalId);
+  }, [navigate]);
+
+  /*useEffect(() => {
+    // Liste des pages en fonction des jours de la semaine (0 = Dimanche, 6 = Samedi)
+    const pages = ["/", "/about", "/contact"]; // Ajouter d'autres pages si nécessaire
+    const today = new Date().getDay(); // Obtenir le jour actuel (0 à 6)
+    const pageToShow = pages[today % pages.length]; // Utiliser modulo pour éviter les erreurs si plus de 7 jours
+
+    // Naviguer vers la page correspondant au jour
+    navigate(pageToShow);
+  }, [navigate]); */
+
   return (
     <div className="app">
       <Navbard />
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/1" element={<Home />} />
+        <Route path="/2" element={<Referentiel />} />
       </Routes>
       <Footer />
     </div>
---
+++ src/component/pages/Referentiel.tsx
@@ -0,0 +1,214 @@
+import React from "react";
+
+const Referentiel = () => {
+  return (
+    <>
+      <div className="" style={{ marginTop: -60 }}>
+        <section className="section-formation-ref-nais">
+          <h2
+            style={{
+              color: "rgba(147, 147, 203, 0.866)",
+              fontStyle: "oblique",
+              marginLeft: 35,
+            }}
+          >
+            Référentiel de Naissance
+          </h2>
+          <br />
+          <div className="d-flex justify-content-around">
+            <div style={{ marginLeft: 5 }}>
+              <img
+                src="/referentiel/refe-img-3.png"
+                alt="."
+                style={{ borderRadius: 10, width: 350, height: 220 }}
+              />
+            </div>
+            <div>
+              <p
+                className="formation-ref-nais-para1"
+                style={{
+                  fontSize: 12,
+                  textAlign: "justify",
+                  marginRight: 5,
+                  marginLeft: 5,
+                }}
+              >
+                <span
+                  className="formation-ref-nais-para1-mot1"
+                  style={{
+                    color: "rgba(147, 147, 203, 0.866)",
+                    fontStyle: "oblique",
+                    fontSize: 15,
+                  }}
+                >
+                  Le référentiel de naissance
+                </span>{" "}
+                en voyance est un outil utilisé par les voyants pour aider à
+                comprendre la personnalité et les traits de caractère d'une
+                personne en se basant sur les informations liées à leur date,
+                heure et lieu de naissance. Cette technique, appelée également
+                astrologie psychologique ou astrologie humaniste, considère que
+                les influences célestes au moment de la naissance peuvent avoir
+                une influence sur la vie et la personnalité d'une personne. Les
+                voyants utilisent le thème natal, qui est un graphique
+                représentant les positions des planètes au moment de la
+                naissance, pour effectuer une interprétation personnalisée de la
+                vie d'une personne et de ses talents, défis et aspirations. Il
+                est important de noter que l'astrologie humaniste est considérée
+                comme une forme de divination et que les conclusions tirées à
+                partir d'un thème natal ne sont pas considérées comme
+                scientifiques ou médicalement fiables. Les interprétations
+                astrologiques doivent être considérées comme des outils de
+                croissance personnelle et de développement de la conscience, et
+                non comme des prédictions infaillibles de l'avenir. Bien sûr, je
+                suis là pour vous aider. Le référentiel de naissance en voyance
+                peut être utilisé pour explorer différents aspects de la vie
+                d'une personne, tels que les relations, la carrière, la santé,
+                les finances, la spiritualité et bien plus encore. En se basant
+                sur les positions des planètes et des signes astrologiques au
+                moment de la naissance, les voyants peuvent aider une personne à
+                mieux comprendre ses motivations profondes, ses talents
+                naturels, ses défis, ses craintes et ses aspirations. Il est
+                important de noter que l'astrologie humaniste ne se concentre
+                pas sur les prédictions futures, mais plutôt sur la
+                compréhension des tendances et des influences qui peuvent être
+                présentes dans la vie d'une personne. Les interprétations
+                astrologiques peuvent offrir une perspective différente sur la
+                vie et aider une personne à prendre des décisions plus
+                conscientes et significatives. En fin de compte, le référentiel
+                de naissance en voyance est un outil personnel qui peut aider à
+                développer la conscience et à explorer les différents aspects de
+                la vie. Il ne doit pas être considéré comme une source
+                définitive de vérité ou de conseils, mais plutôt comme un moyen
+                d'ouvrir l'esprit à de nouvelles perspectives et de mieux
+                comprendre soi-même et les autres.
+              </p>
+            </div>
+          </div>
+          <br />
+          <div className="d-flex justify-content-around">
+            <div>
+              <p
+                className="formation-ref-nais-para2"
+                style={{
+                  fontSize: 13,
+                  textAlign: "justify",
+                  marginRight: 5,
+                  marginLeft: 5,
+                }}
+              >
+                Tout comme nous utilisons un miroir pour ordonner notre image
+                physique, le Référentiel est un miroir qui nous permet
+                d’ordonner notre image intérieure souvent malmenée par nos
+                différentes expériences. Au travers de symboles et d’archétypes,
+                le miroir du Référentiel reflète 14 parts de nous-mêmes. Pour
+                établir son Référentiel, il est nécessaire de faire coïncider 14
+                images ou symboles par un calcul spécifique avec les 14 zones du
+                Référentiel. Ces images sont ensuite disposées le long des
+                quatre axes composant la structure du Référentiel.
+              </p>
+
+              <ul
+                className="formation-ref-nais-para2-ul"
+                style={{
+                  fontSize: 16,
+                  textAlign: "justify",
+                  marginRight: 5,
+                  marginLeft: 5,
+                }}
+              >
+                <div className="d-flex justify-content-between">
+                  <div className="">
+                    <li>
+                      <span className="formation-ref-nais-para2-span">
+                        Zone 1 : mon image sociale
+                      </span>
+                    </li>
+                    <li>
+                      <span className="formation-ref-nais-para2-span">
+                        Zone 2 : ma quête
+                      </span>
+                    </li>
+                    <li>
+                      <span className="formation-ref-nais-para2-span">
+                        Zone 3 : ce qui préoccupe ma pensée
+                      </span>
+                    </li>
+                    <li>
+                      <span className="formation-ref-nais-para2-span">
+                        Zone 4 : comment orienter mon existence
+                      </span>
+                    </li>
+                    <li>
+                      <span className="formation-ref-nais-para2-span">
+                        Zone 5 : ce que je répète dans ma vie
+                      </span>
+                    </li>
+                    <li>
+                      <span className="formation-ref-nais-para2-span">
+                        Zone 6 : mes ressources
+                      </span>
+                    </li>
+                    <li>
+                      <span className="formation-ref-nais-para2-span">
+                        Zone 7 : mes défis
+                      </span>
+                    </li>
+                  </div>
+                  <div className="">
+                    <li>
+                      <span className="formation-ref-nais-para2-span">
+                        Zone 8 : les énergies dont je dispose durant une année
+                      </span>
+                    </li>
+                    <li>
+                      <span className="formation-ref-nais-para2-span">
+                        Zone 9 : mon terrain d’excellence
+                      </span>
+                    </li>
+                    <li>
+                      <span className="formation-ref-nais-para2-span">
+                        Zone 10 : mes expériences et/ou mes échecs
+                      </span>
+                    </li>
+                    <li>
+                      <span className="formation-ref-nais-para2-span">
+                        Zone 11 : le projet parental inconscient
+                      </span>
+                    </li>
+                    <li>
+                      <span className="formation-ref-nais-para2-span">
+                        {" "}
+                        Zone 12 : mon idéal
+                      </span>
+                    </li>
+                    <li>
+                      <span className="formation-ref-nais-para2-span">
+                        Zone 13 : mes paradoxes
+                      </span>
+                    </li>
+                    <li>
+                      <span className="formation-ref-nais-para2-span">
+                        Zone 14 : mes ressources complémentaires
+                      </span>
+                    </li>
+                  </div>
+                </div>
+              </ul>
+            </div>
+            <div>
+              <img
+                className="formation-ref-nais-imag2"
+                src="/referentiel/refe-img-2.png"
+                alt="."
+                style={{ borderRadius: 10, width: 500, height: 300 }}
+              />
+            </div>
+          </div>
+        </section>
+      </div>
+    </>
+  );
+};
+
+export default Referentiel;
