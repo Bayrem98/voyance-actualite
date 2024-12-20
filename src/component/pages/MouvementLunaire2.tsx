29794d0fda7fca61a2ddd92ba61b1319990aed21
bayrem98 <bayremsayeh98@gmail.com>
2024-12-20 05:49:45 +0100

    update & changed contenu of page two & remove calender images

--- src/component/pages/MouvementLunaire2.tsx
+++ src/component/pages/MouvementLunaire2.tsx
@@ -1,34 +1,57 @@
 const MouvementLunaire2 = () => {
   return (
     <>
-      <div className="" style={{ marginTop: -70 }}>
-        <div className="d-flex justify-content-between">
-          <div>
-            {" "}
+      <div className="" style={{ marginTop: 3 }}>
+        <div
+          className="d-flex justify-content-between"
+          style={{ marginLeft: 50 }}
+        >
+          <div className="">
             <img
               className="calend-plein"
-              src="/lune/calendrier.jpg"
+              src="/lune/image-pleine-lune2024.jpg"
               alt="."
               style={{
-                borderRadius: 50,
-                width: 670,
-                height: 650,
-                marginLeft: 10,
+                borderRadius: 15,
+                width: 750,
+                height: 530,
               }}
             />
           </div>
-          <div>
-            <img
-              className="calend-nouvelle"
-              src="/lune/calendrier-2.jpg"
-              alt="."
-              style={{
-                borderRadius: 50,
-                width: 670,
-                height: 650,
-                marginRight: 10,
-              }}
-            />
+          <div className="" style={{ marginRight: 80, marginTop: 50 }}>
+            <p>
+              <span
+                style={{ fontSize: 30, color: "rgba(147, 147, 203, 0.866)" }}
+              >
+                Phases de la Lune pour décembre 2024
+              </span>
+              <br />
+              <br />
+              <br />
+              <span style={{ fontSize: 22 }}>
+                Nouvelle Lune : 1er décembre, 06:21 GMT{" "}
+              </span>
+              <br />
+              <br />
+              <span style={{ fontSize: 22 }}>
+                Premier Quartier : 8 décembre, 15:27 GMT
+              </span>
+              <br />
+              <br />
+              <span style={{ fontSize: 22 }}>
+                Pleine Lune : 15 décembre, 09:02 GMT
+              </span>
+              <br />
+              <br />
+              <span style={{ fontSize: 22 }}>
+                Dernier Quartier : 22 décembre, 22:18 GMT
+              </span>
+              <br />
+              <br />
+              <span style={{ fontSize: 22 }}>
+                Nouvelle Lune : 30 décembre, 22:27 GMT
+              </span>
+            </p>
           </div>
         </div>
       </div>
